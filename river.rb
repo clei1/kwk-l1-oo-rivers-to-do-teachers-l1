6dfc6d66162e3566c2fb1ed1d254a855c527bea0
clei1 <callmecon328@gmail.com>
2018-07-21 17:46:43 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,19 @@
 # river.rb
+
+class River
+
+  attr_accessor :name, :length, :countries, :discharge
+
+  def initialize(n)
+    @name = n
+  end
+
+  def flood
+    @discharge *= 1.3
+  end
+
+  def dry_up
+    @discharge *= 0.5
+  end
+
+end
